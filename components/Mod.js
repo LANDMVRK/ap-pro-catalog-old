1a40743eac3b7dc21bdf9f0fbce262f24c874030
LANDMVRK <76461039+LANDMVRK@users.noreply.github.com>
2021-06-18 19:24:31 +0500

    Add files via upload

---
+++ components/Mod.js
@@ -0,0 +1,31 @@
+function Mod(props) {
+    const { Url, Title, PicURL, Description, Tags, Views, ReleaseDate, Rating } = props.mod
+    const ratingColor = Rating >= 7 ? 'lightgreen' : (Rating >= 5 ? 'gold' : 'indianred')
+    const displayedRating = Rating.toString().replace('.', ',')
+    const displayedViews = Views > 9999 ? new Intl.NumberFormat('ru-ru').format(Views) : Views
+    return (
+      <div className="mod">
+        <a className="mod__title" href={Url}>{Title}</a>
+        <div className="mod__flex-govno">
+          <div className="mod__preview-wrapper">
+            <img className="mod__preview" src={PicURL} loading="lazy" />
+          </div>
+          <div className="mod__description">
+            {Description}
+            <div className="mod__tags">
+            {
+              Tags.map(function(tag) {
+                return <div className="mod__tags-item">{tag}</div>
+              })
+            }
+            </div>
+          </div>
+        </div>
+        <div className="mod__bottom-item">Рейтинг: <span style={{color: ratingColor}}>{displayedRating}</span></div>
+        <div className="mod__bottom-item">Просмотры: {displayedViews}</div>
+        <div className="mod__bottom-item">Опубликован: {ReleaseDate}</div>
+      </div>
+    )
+}
+
+export default Mod
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import '../styles/vars.css'
+import '../styles/index.css'
+import '../styles/mod.css'
+import '../styles/buttons.scss'
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
