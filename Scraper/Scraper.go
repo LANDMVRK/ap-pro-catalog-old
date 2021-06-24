6e29b5913f3aabc590f7e08d361a43d15010be97
LANDMVRK <76461039+LANDMVRK@users.noreply.github.com>
2021-06-25 00:14:54 +0500

    Add files via upload

---
+++ Scraper/Scraper.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+	"log"
+	"strconv"
+	"fmt"
+	"time"
+	"regexp"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+
+  	"github.com/PuerkitoBio/goquery"
+)
+
+type Mod struct {
+	Title string
+	PicURL string
+	Authors string
+	ReleaseDate string
+	Views int
+	Rating float64
+	Reviews int
+	Description string
+	Video bool
+	Guide bool
+	Screens bool
+	Review bool
+	Tags []string
+	Platform string
+	Url string
+}
+
+type Result struct {
+	Scraped int64
+	Data []Mod
+}
+
+func main() {
+	// 1. Узнаем количество страниц в каталоге.
+	root := getDocument("https://ap-pro.ru/stuff/")
+	pagesNum, _ := strconv.Atoi(root.Find(".ipsPagination_last a").AttrOr("data-page", ""))
+	// 2. Генерируем ссылки на страницы каталога.
+	catalogUrls := make([]string, pagesNum)
+	for i := range catalogUrls {
+		catalogUrls[i] = fmt.Sprint("https://ap-pro.ru/stuff/page/", i + 1)
+	}
+	// 3. Получаем содержимое страниц каталога.
+	fmt.Println("Получение содержимого страниц каталога...")
+	catalogContent := getDocuments(catalogUrls)
+	// 4. Извлекаем ссылки на страницы модов со страниц каталога.
+	fmt.Println("Извлечение ссылок на страницы модов со страниц каталога...")
+	var modUrls []string
+	for _, doc := range catalogContent {
+		doc.Find("header h2 a[title^=\"Подробнее о\"]").Each(func(i int, s *goquery.Selection) {
+			modUrls = append(modUrls, s.AttrOr("href", ""))
+		})
+	}
+	fmt.Println("Найдено модов:", len(modUrls))
+	// 5. Получаем содержимое страниц модов.
+	fmt.Println("Получение содержимого страниц модов...")
+	scraped := time.Now()
+	modsContent := getDocuments(modUrls)
+	fmt.Println("Заняло времени:", time.Since(scraped))
+	// 6. Извлекаем инфу со страниц модов.
+	fmt.Println("Извлечение инфы со страниц модов...")
+	parsingStarted := time.Now()
+	data := make([]Mod, len(modsContent))
+	var wg sync.WaitGroup
+	for i, doc := range modsContent {
+		wg.Add(1)
+		go func(i int, doc *goquery.Document) {
+			defer wg.Done()
+			var mod Mod
+			mod.Title = doc.Find(".ipsPageHeader .ipsType_pageTitle span").Text()
+
+			picStyle, _ := doc.Find(".modInfoGrid .cCmsRecord_image").Attr("style")
+			picURL := strings.Split(picStyle, "url(")[1]
+			mod.PicURL = strings.Split(picURL, ")")[0]
+
+			mod.Authors = strings.TrimSpace(doc.Find(".modInfoGrid .fa-user").Parent().Text())
+			mod.ReleaseDate = strings.TrimSpace(doc.Find(".modInfoGrid .fa-clock-o").Parent().Text())
+
+			modInfoGridText := doc.Find(".modInfoGrid").Text()
+
+			re := regexp.MustCompile(`(\d+) \| Позиция в рейтинге`)
+			views, _ := strconv.Atoi(re.FindStringSubmatch(modInfoGridText)[1])
+			mod.Views = views
+
+			re = regexp.MustCompile(`(\d{1,2}(?:,\d)?)[^\d]+(\d+) отзыв`)
+			matches := re.FindStringSubmatch(modInfoGridText)
+			rating, _ := strconv.ParseFloat(strings.ReplaceAll(matches[1], ",", "."), 64)
+			reviews, _ := strconv.Atoi(matches[2])
+			mod.Rating = rating
+			mod.Reviews = reviews
+
+			mod.Description = strings.TrimSpace(doc.Find("article section p").Text())
+
+			addButtonsText := doc.Find("article .additionalButtons").Text()
+			mod.Video = strings.Contains(addButtonsText, "Видео")
+			mod.Guide = strings.Contains(addButtonsText, "Прохожден")
+			mod.Screens = strings.Contains(addButtonsText, "Скрин")
+			mod.Review = strings.Contains(addButtonsText, "Обзор")
+
+			mod.Platform = strings.TrimSpace(doc.Find(".modInfoGrid .fa-folder-open-o").Parent().Text())
+
+			doc.Find(".ipsTags.ipsList_inline li span").Each(func(i int, s *goquery.Selection) {
+				mod.Tags = append(mod.Tags, s.Text())
+			})
+
+			mod.Url = modUrls[i]
+
+			data[i] = mod
+		}(i, doc)
+	}
+	wg.Wait()
+	fmt.Println("Заняло времени:", time.Since(parsingStarted))
+	// 7. Записываем результат на диск.
+	file, _ := json.MarshalIndent(Result{scraped.Unix(), data}, "", "    ")
+	// Приложение использует этот файл.
+	_ = ioutil.WriteFile("../data.json", file, 0644)
+	// Для истории.
+	n := fmt.Sprintf("../../Data-%d-%02d-%02d-%02d-%02d-%02d.json", scraped.Year(), scraped.Month(), scraped.Day(), scraped.Hour(), scraped.Minute(), scraped.Second())
+	_ = ioutil.WriteFile(n, file, 0644)
+	
+	fmt.Println("Всё. Результат в файле", n)
+
+	// 8. Выполняем дополнительные действия.
+
+	tagsSet := make(map[string]bool)
+	platformSet := make(map[string]bool)
+	for _, mod := range data {
+		platformSet[mod.Platform] = true
+		for _, tag := range mod.Tags {
+			tagsSet[tag] = true
+		}
+	}
+	var tagsArr []string
+	for k := range tagsSet {
+		tagsArr = append(tagsArr, k)
+	}
+	var platformArr []string
+	for k := range platformSet {
+		platformArr = append(platformArr, k)
+	}
+	file, _ = json.MarshalIndent(tagsArr, "", "    ")
+	_ = ioutil.WriteFile("../tags.json", file, 0644)
+	file, _ = json.MarshalIndent(platformArr, "", "    ")
+	_ = ioutil.WriteFile("../platforms.json", file, 0644)
+
+	fmt.Println("Скачивание превьюшек...")
+	lalka := time.Now()
+	for i, mod := range data {
+		wg.Add(1)
+		go func(i int, url string) {
+			defer wg.Done()
+			time.Sleep(time.Duration(i) * 100 * time.Millisecond)
+			savePic(url, "../public/previews/")
+		}(i, mod.PicURL)
+	}
+	wg.Wait()
+	fmt.Println("Заняло времени:", time.Since(lalka))
+}
+
+func getDocuments(urls []string) []*goquery.Document {
+	// https://stackoverflow.com/questions/49879322/can-i-concurrently-write-different-slice-elements
+	// https://www.reddit.com/r/golang/comments/jxr0o3/are_fixedlength_arrays_threadsafe_if_goroutines/
+	docs := make([]*goquery.Document, len(urls))
+	var wg sync.WaitGroup
+	for i, url := range urls {
+		wg.Add(1)
+		go func(i int, url string) {
+			defer wg.Done()
+			time.Sleep(time.Duration(i) * 100 * time.Millisecond)
+			docs[i] = getDocument(url)
+		}(i, url)
+	}
+	wg.Wait()
+	return docs
+}
+
+func getDocument(url string) *goquery.Document {
+	res, err := http.Get(url)
+	check(err)
+	if res.StatusCode != 200 {
+		log.Fatalln("status code error:", url, res.Status)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	check(err)
+	return doc
+}
+
+func savePic(url, folder string) {
+	res, err := http.Get(url)
+	check(err)
+	if res.StatusCode != 200 {
+		log.Fatalln("status code error:", url, res.Status)
+	}
+	defer res.Body.Close()
+	content, err := ioutil.ReadAll(res.Body)
+	check(err)
+	kek := strings.Split(url, "/")
+	err = ioutil.WriteFile(folder + kek[len(kek) - 1], content, 0644)
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
